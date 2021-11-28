fbc176c7913b83a33dd3cf6828c697a95d9ccb4b
KHJcode <kimstudswcode4485@gmail.com>
2021-11-28 19:34:59 +0900

    set eslint prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import fastify from "fastify";
-import multer from 'fastify-multer';
-import { v4 } from 'uuid';
+import multer from "fastify-multer";
+import { v4 } from "uuid";
 import celebrityRouter from "./routes/celebrity";
 import fastifyCors from "fastify-cors";
-import fastifyStatic from 'fastify-static';
+import fastifyStatic from "fastify-static";
 import path from "path";
 
 const server = fastify();
@@ -11,33 +11,33 @@ const PORT = process.env.PORT || 8080;
 
 server.register(multer.contentParser);
 server.register(fastifyStatic, {
-  root: path.join(__dirname, '../uploads'),
-  prefix: '/uploads/',
+  root: path.join(__dirname, "../uploads"),
+  prefix: "/uploads/",
 });
 server.register(fastifyCors, { origin: true });
 server.register(celebrityRouter, { prefix: "/celebrity" });
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, "uploads/");
   },
   filename(req, file, cb) {
     cb(null, `${v4()}.jpg`);
-  }
+  },
 });
 const upload = multer({ storage });
 
 server.route({
-  method: 'POST',
-  url: '/upload',
-  preHandler: upload.single('photo'),
+  method: "POST",
+  url: "/upload",
+  preHandler: upload.single("photo"),
   handler: (request, reply) => {
-    reply.code(200).send('SUCCESS');
-  }
+    reply.code(200).send("SUCCESS");
+  },
 });
 
-server.get('/', async (request, reply) => {
-  reply.code(200).send('Hello, World!');
+server.get("/", async (request, reply) => {
+  reply.code(200).send("Hello, World!");
 });
 
 server.listen(PORT, (err, address) => {
